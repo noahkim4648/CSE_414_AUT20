48621fce0d9fee98f3f2244d40f138875a040c63
Noah Kim <Noahkim4648@gmail.com>
2020-11-05 21:30:40 -0800

    'gone'

--- import-tables.sql
+++
@@ -1,8 +0,0 @@
-/* Importing the data into their respective tables */
-
-.mode csv
-
-.import flights-small.csv FLIGHTS
-.import carriers.csv CARRIERS
-.import months.csv MONTHS
-.import weekdays.csv WEEKDAYS
